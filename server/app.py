b096479f2eb19f447fd7a5b4e2ab9eca9daadf43
Tristan Macell <tristanmacelli@gmail.com>
2020-04-12 14:46:08 -0700

    [ADD] Flask import to app.py

--- server/app.py
+++ server/app.py
@@ -1,15 +1,21 @@
 from TwilioIntegration import 
 from YoutubeIntegration import 
 from MessageQueue import 
+from flask import Flask, request
+from flask_api import status
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 if __name__ == '__main__':
     app.run(debug=True, host='streams', port="8080")
 
 @app.route("/v1/stream/", methods=['POST'])
 def beginStream():
-    # CALL mock L1 function
+    # CALL mock Twilio Integration function
+    
     # CALL mock MQ function
-    # CALL mock L2 function
+    # CALL mock YT Integration function
 
 
 @app.route("/v1/stream/", methods=['DELETE'])
