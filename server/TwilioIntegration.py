43244610d30464c269fa1b9c433c36c906512b30
Tristan Macell <tristanmacelli@gmail.com>
2020-04-15 13:12:00 -0700

    [REMOVE] class keyword and Twilio GET function

--- server/TwilioIntegration.py
+++ server/TwilioIntegration.py
@@ -1,50 +1,15 @@
 import requests
-import pika
 
 # GETting SMS messages from Twilio & sending to MQ
-class TwilioIntegration:
-    def __init__(self, authToken):
-        self.authToken = authToken
 
-    # Authenticate w/ Twilio & store token in Token Store (S3)
-    def TwilioAuth():
-        
-    # Gets Twilio auth token from Token Store (S3) to send requests
-    def GetStoredToken():
 
-    # GET request to Twillio for messages
-    def GETTwilioMessages():
-        # IF token is expired,
-        if ():
-            # re-authenticate with Twilio
-            # Store auth token in Token Store (S3)
+# Reshape data
+def ShapeMessage(message):
+    print(message)
+    return message.body
 
-        badStatus = True
-        URL = "URL for Twilio"
-        PARAMS = {'param_key':param_value} 
-    
-        while badStatus:
-            # sending GET request for Twilio messages
-            response = requests.GET(url = URL, params = PARAMS) 
-            data = response.json()
-            # TODO: Check that this is the proper way to check response status
-            if (data["status"] < 400 & data["status"] > 199):
-                badStatus = False
 
-    # Reshape data
-    def ShapeMessages():
-
-    # Send messages to MQ
-    def ProduceMessages(channel, messages):
-        for message in messages:
-            channel.basic_publish(exchange='', routing_key='youtube-messages', body='{message}')
-
-    def ProcessMessages():
-        # Connect to MQ
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-        channel = connection.channel()
-        channel.queue_declare(queue='youtube-messages')
-        
-        messages = GETTwilioMessages()
-        final_messages = ShapeMessages(messages)
-        ProduceMessages(channel, final_messages)
+def ProcessMessages(event, context):
+    print(event)
+    final_message = ShapeMessage(event.message)
+    print(final_message)
--- server/YoutubeIntegration.py
+++ server/YoutubeIntegration.py
@@ -1,4 +1,3 @@
-import pika
 import os
 
 import google_auth_oauthlib.flow
@@ -8,83 +7,67 @@ import googleapiclient.errors
 scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
 # Consuming messages from MQ & POSTING to YT Live
-class YoutubeIntegration:
-    def __init__(self, authToken):
-        self.authToken = authToken
-        
-    # Authenticate w/ YT & store token in Token Store (S3)
-    def YoutubeAuth():
-        # Code from: https://developers.google.com/youtube/v3/live/docs/liveChatMessages/insert?apix=true#http-request
-        # Disable OAuthlib's HTTPS verification when running locally.
-        # *DO NOT* leave this option enabled in production.
-        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-        api_service_name = "youtube"
-        api_version = "v3"
-        client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
 
-        # Get credentials and create an API client
-        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-            client_secrets_file, scopes)
-        credentials = flow.run_console()
-        youtube = googleapiclient.discovery.build(
-            api_service_name, api_version, credentials=credentials)
-        
-    # Gets YT auth token from Token Store (S3 in our case) to send requests
-    def GetStoredToken():
-        # TODO: implement this method
+# Authenticate w/ YT & store token in Token Store (S3)
+def YoutubeAuth():
+    # Code from: https://developers.google.com/youtube/v3/live/docs/liveChatMessages/insert?apix=true#http-request
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-    # SEND POST Requests to YT Live
-    def POSTMessage(ch, method, properties, body):
-        # IF token is expired,
-        if ():
-            # re-authenticate with YT
-            # Store auth token in Token Store (S3)
-        badStatus = True
-        name, body = ShapeMessage(body)
-        while badStatus:
-            # sending POST request, inserting messages into LiveChat
-            request = youtube.liveChatMessages().insert(
-                part="snippet",
-                body={
-                "snippet": {
-                    "liveChatId": "YOUR_LIVE_CHAT_ID",
-                    "type": "textMessageEvent",
-                    "textMessageDetails": {
-                    "messageText": "{body}"
-                    }
-                },
-                "authorDetails": {
-                    "displayName": "{name}"
-                },
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
+
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+# Is this necessary
+# Gets YT auth token from Token Store (S3 in our case) to send requests
+def GetStoredToken():
+    # TODO: implement this method
+    print("Stored Token Got")
+
+# def callback(ch, method, properties, body):
+#     print(" [x] Received %r" % body)
+
+
+# Reshape data
+def ShapeMessage(body):
+    splitBody = body.split(" ", 1)
+    name = splitBody[0]
+    message = splitBody[1]
+    return name, message
+
+# SEND POST Requests to YT Live
+def ProcessMessages():
+    badStatus = True
+    name, body = ShapeMessage(body)
+    # sending POST request, inserting messages into LiveChat
+    request = youtube.liveChatMessages().insert(
+        part="snippet",
+        body={
+            "snippet": {
+                "liveChatId": "YOUR_LIVE_CHAT_ID",
+                "type": "textMessageEvent",
+                "textMessageDetails": {
+                    "messageText": f"{body}"
                 }
-            )
-            response = request.execute()
-            data = response.json()
-            # TODO: Check that this is the proper way to check response status
-            if (data["status"] < 400 & data["status"] > 199):
-                badStatus = False
-            else:
-                # re-authenticate to generate auth token
-
-    # 
-    # def callback(ch, method, properties, body):
-    #     print(" [x] Received %r" % body)
-
-    # Reshape data
-    def ShapeMessage(body):
-        splitBody = body.split(" ", 1)
-        name = splitBody[0]
-        message = splitBody[1]
-        return name, message
-
-    def ProcessMessages():   
-        # Connect to MQ
-        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-        channel = connection.channel()
-        channel.queue_declare(queue='youtube-messages')
-        channel.basic_consume(queue='youtube-messages',
-                    auto_ack=True,
-                    on_message_callback=POSTMessage)
-        # The following starts an infinite loop that waits for & consumes messages
-        channel.start_consuming()
+            },
+            "authorDetails": {
+                "displayName": f"{name}"
+            },
+        })
+    response = request.execute()
+    data = response.json()
+    # TODO: Check that this is the proper way to check response status
+    if (data["status"] < 400 & data["status"] > 199):
+        # Message was received by Youtube therefore we can continue
+    else:
+        # Send message to secondary location [comment section of last live stream]
+        print("Sending messages to comment section of most recent live stream")
