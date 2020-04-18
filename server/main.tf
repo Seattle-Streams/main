6f39de00545b0c04f406e86bfc02ecd4dae41312
Tristan Macell <tristanmacelli@gmail.com>
2020-04-17 17:42:20 -0700

    [CHANGE] move tfstate to remote backend

--- server/main.tf
+++ server/main.tf
@@ -2,3 +2,14 @@ provider "aws" {
   profile = "devops"
   region  = "${var.region}"
 }
+
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "Seattle-Streams"
+
+    workspaces {
+      name = "main"
+    }
+  }
+}
