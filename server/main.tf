a0d736a2ac136d30e7b54a28e5cc24da07f1ee12
Tristan Macell <tristanmacelli@gmail.com>
2020-04-17 18:09:32 -0700

    [ADD] aws env vars to terraform remote

--- server/main.tf
+++ server/main.tf
@@ -1,6 +1,8 @@
 provider "aws" {
-  profile = "devops"
-  region  = "${var.region}"
+  profile    = "devops"
+  access_key = "${var.AWS_ACCESS_KEY_ID}"
+  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
+  region     = "${var.region}"
 }
 
 terraform {
