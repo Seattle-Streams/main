8fcff18f2b3f26bbef5154f8366fa757fbe03288
Tristan Macell <tristanmacelli@gmail.com>
2020-04-28 20:13:35 -0700

    [CHANGE] EC2 ami to be Amazon Linux 2

--- server/build.tf
+++ server/build.tf
@@ -1,6 +1,6 @@
 # Jenkins EC2
 resource "aws_instance" "server" {
-  ami             = "ami-4b32be2b"
+  ami             = "ami-0d6621c01e8c2de2c"
   instance_type   = "t2.micro"
   key_name        = "${aws_key_pair.Jenkins_CI.key_name}"
   security_groups = ["${aws_security_group.allow_http.name}"]
