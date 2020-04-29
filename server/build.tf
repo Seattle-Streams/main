33d24879e7d575ad9cfb863cb901c81f3fdb3014
Tristan Macell <tristanmacelli@gmail.com>
2020-04-29 01:26:00 -0700

    [REMOVE] jenkins service start from EC2 provisioning

--- server/build.tf
+++ server/build.tf
@@ -21,7 +21,6 @@ resource "aws_instance" "server" {
       "sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo",
       "sudo rpm --import http://pkg.jenkins.io/redhat/jenkins.io.key",
       "sudo yum install jenkins --y",
-      "sudo service jenkins start"
     ]
   }
 }
