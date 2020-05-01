9f5a48775dcf15f6f8eb55eeae1d736e7771deda
Tristan Macell <tristanmacelli@gmail.com>
2020-05-01 00:09:09 -0700

    [CHANGE] re-link youtube lambda to S3

--- server/lambdas.tf
+++ server/lambdas.tf
@@ -55,9 +55,8 @@ resource "aws_lambda_function" "twilio_lambda" {
 resource "aws_lambda_function" "youtube_lambda" {
   function_name = "youtube_lambda"
 
-  filename = "youtube_lambda.zip"
-  #   s3_bucket = "process-messages-builds"
-  #   s3_key    = "youtube_lambda.zip"
+  s3_bucket = "process-messages-builds"
+  s3_key    = "youtube_lambda.zip"
 
   role    = "${aws_iam_role.iam_lambda_execution_role.arn}"
   handler = "YoutubeIntegration.ProcessMessage"
