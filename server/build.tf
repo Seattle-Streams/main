5754212b1e40c38eaa9453652cddf0a39bb0ccc3
Tristan Macell <tristanmacelli@gmail.com>
2020-04-30 19:29:07 -0700

    [ADD] https support for Jenkins EC2

--- server/build.tf
+++ server/build.tf
@@ -54,6 +54,14 @@ resource "aws_security_group" "jenkins_management" {
   }
 
   ingress {
+    description = "Allow https connections"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "Allow Jenkins connections"
     from_port   = 8080
     to_port     = 8080
