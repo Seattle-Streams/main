309375ea5794ce86ae2e087aeb2ed5d6304e7942
Tristan Macell <tristanmacelli@gmail.com>
2020-05-04 11:46:25 -0700

    [ADD] BUCKET_NAME env variable for flexibility

--- server/youtube/YoutubeIntegration.py
+++ server/youtube/YoutubeIntegration.py
@@ -11,7 +11,10 @@ from oauth2client import tools
 from oauth2client.file import Storage
 import googleapiclient.errors
 
-SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl", "https://www.googleapis.com/auth/youtube.readonly"]
+SCOPES = [
+    "https://www.googleapis.com/auth/youtube.force-ssl",
+    "https://www.googleapis.com/auth/youtube.readonly"
+]
 API_SERVICE_NAME = "youtube"
 API_VERSION = "v3"
 # CLIENT_SECRET_FILE = "client_secret.json"
@@ -19,39 +22,41 @@ CREDENTIAL_FILE = "credentials.json"
 # AUTH_CODE = ""
 # Remember to verify authenticity of auth code!
 # https://developers.google.com/identity/sign-in/web/backend-auth
-# use verify_oauth2_token 
+# use verify_oauth2_token
 
 # MESSAGE = "Hello World!!"
 
 s3 = boto3.client('s3')
-BUCKET_NAME = 'process-messages-builds'
+BUCKET_NAME = os.environ['BUCKET_NAME']
+
 
 # getLiveChatID gets the liveChatID of the currently streaming broadcast
 def getLiveChatID(youtubeObject) -> str:
     request = youtubeObject.liveBroadcasts().list(
-        part="snippet", # available: snippet, status broadcastContent (spelling?)
+        part=
+        "snippet",  # available: snippet, status broadcastContent (spelling?)
         broadcastType="all",
-        mine=True # only the broadcasts corresponding to authenticated user
+        mine=True  # only the broadcasts corresponding to authenticated user
     )
     response = request.execute()
     # TODO: sort this list
     liveChatID = response['items'][0]['snippet']['liveChatId']
     return liveChatID
 
+
 # postMessage inserts the specified message into the livechat corresponding with the given liveChatID
 def postMessage(youtubeObject, liveChatID, message) -> str:
     request = youtubeObject.liveChatMessages().insert(
         part="snippet",
         body={
-          "snippet": {
-            "liveChatId": liveChatID,
-            "type": "textMessageEvent",
-            "textMessageDetails": {
-              "messageText": message
+            "snippet": {
+                "liveChatId": liveChatID,
+                "type": "textMessageEvent",
+                "textMessageDetails": {
+                    "messageText": message
+                }
             }
-          }
-        }
-    )
+        })
     response = request.execute()
 
     return response
@@ -59,28 +64,28 @@ def postMessage(youtubeObject, liveChatID, message) -> str:
 
 # for now, gets credentials if they exist, or breaks
 def getStoredCredentials():
-    
-    # pull credentials from S3    
+
+    # pull credentials from S3
     local_file_name = "/tmp/" + CREDENTIAL_FILE
     s3.download_file(BUCKET_NAME, CREDENTIAL_FILE, local_file_name)
-    
+
     store = Storage(local_file_name)
-    
+
     credentials = store.locked_get()
     # if not credentials or credentials.invalid:
 
-        # Storage object  class:
-        # https://oauth2client.readthedocs.io/en/latest/_modules/oauth2client/file.html#Storage
-        # Set store with credentials.set_store(store):
-        # https://oauth2client.readthedocs.io/en/latest/_modules/oauth2client/client.html#OAuth2Credentials.set_store
-        
-        # note: authcode is currently set
-        # Exchange auth code for access token, refresh token, and ID token
-        # credentials = client.credentials_from_clientsecrets_and_code(
-        #     CLIENT_SECRET_FILE,
-        #     SCOPES,
-        #     AUTH_CODE)
-        # store.locked_put(credentials)
+    # Storage object  class:
+    # https://oauth2client.readthedocs.io/en/latest/_modules/oauth2client/file.html#Storage
+    # Set store with credentials.set_store(store):
+    # https://oauth2client.readthedocs.io/en/latest/_modules/oauth2client/client.html#OAuth2Credentials.set_store
+
+    # note: authcode is currently set
+    # Exchange auth code for access token, refresh token, and ID token
+    # credentials = client.credentials_from_clientsecrets_and_code(
+    #     CLIENT_SECRET_FILE,
+    #     SCOPES,
+    #     AUTH_CODE)
+    # store.locked_put(credentials)
 
     # unclear what the value of this line is
     credentials.set_store(store)
@@ -90,7 +95,7 @@ def getStoredCredentials():
 # auth authenticates with the provided client secrets file, scope, and authorization code
 # returns youtube client object
 def auth():
-    
+
     # TODO: implement in api gateway
     # (Receive auth_code by HTTPS POST)
 
@@ -99,7 +104,6 @@ def auth():
     # if not request.headers.get('X-Requested-With'):
     #     abort(403)
 
-    
     # TODO: Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -112,12 +116,12 @@ def auth():
     if credentials.access_token_expired:
         credentials.refresh(httplib2.Http())
         httpAuth = credentials.authorize(httplib2.Http())
-        
 
     youtubeService = discovery.build(
         API_SERVICE_NAME, API_VERSION, http=httpAuth)
 
     return youtubeService
+
 
 def ProcessMessage(event, context):
     message = event["body"]
