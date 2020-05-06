e3ad7c067e9f21ba92153df5d656854687b66dc6
Tristan Macell <tristanmacelli@gmail.com>
2020-05-06 16:31:28 -0700

    [ADD] Receiving number parsing

--- server/twilio/Integration.py
+++ server/twilio/Integration.py
@@ -12,15 +12,26 @@ sqs = boto3.client('sqs')
 def ShapeMessage(message):
     message = message.split("Body=", 1)[1]
     message = message.split("&FromCountry", 1)[0]
+    message.replace("+", " ")
     message = unquote(message)
     return message
 
+def GetNumber(message):
+    number = message.split("To=", 1)[1]
+    number = number[:20]
+    return number
 
 # ProcessMessage extracts the sms message from the request and sends it to an SQS Queue
 def ProcessMessage(event, context):
+    number = GetNumber(event['body'])
+    print("Logging Twilio Receiving number: ", number)
     message = ShapeMessage(event['body'])
 
     print("Logging SMS Message: ", message)
-    response = sqs.send_message(QueueUrl=QUEUE_URL, MessageBody=message)
+    response = sqs.send_message(QueueUrl=QUEUE_URL, MessageBody=message, 
+    MessageAttributes={'receiving-number': {
+        'StringValue': '{number}',
+        'DataType': 'String'
+    }})
     print("Logging SQS Response: ", response)
-    return {'statusCode': 200}
+    # return {'statusCode': 200}
