39a19a411e2160c674cad86d1cd289c3867978a3
Tristan Macell <tristanmacelli@gmail.com>
2020-05-06 17:13:45 -0700

    [REFACTOR] url parsing using parse_qs to convert the request to JSON

--- server/twilio/Integration.py
+++ server/twilio/Integration.py
@@ -1,39 +1,24 @@
 import json
 import os
 import boto3
-from urllib.parse import unquote
+from urllib.parse import parse_qs
 
 QUEUE_URL = os.environ['SQS_URL']
 
 sqs = boto3.client('sqs')
 
-
-# Reshape data parses the incoming message and returns the client message
-def ShapeMessage(message):
-    message = message.split("Body=", 1)[1]
-    message = message.split("&FromCountry", 1)[0]
-    message = message.replace("+", " ")
-    message = unquote(message)
-    return message
-
-# GetNumber parses the incoming message and returns the receiving Twilio phone number
-def GetNumber(message):
-    number = message.split("To=", 1)[1]
-    number = number.split("&MessagingServiceSid", 1)[0]
-    number = unquote(number)
-    return number
-
 # ProcessMessage extracts the sms message from the request and sends it to an SQS Queue
 def ProcessMessage(event, context):
-    number = GetNumber(event['body'])
-    print("Logging Twilio Receiving Number: ", number)
-    message = ShapeMessage(event['body'])
+    requestJSON = parse_qs(event['body'])
+    number = requestJSON['To']
+    message = requestJSON['Body']
 
-    print("Logging SMS Message: ", message)
     response = sqs.send_message(QueueUrl=QUEUE_URL, MessageBody=message, 
     MessageAttributes={'receiving-number': {
         'StringValue': '{number}',
         'DataType': 'String'
     }})
+    print("Logging Twilio Receiving Number: ", number)
+    print("Logging SMS Message: ", message)
     print("Logging SQS Response: ", response)
     # return {'statusCode': 200}
