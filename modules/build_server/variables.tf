1d7526525602486cc891b345f67164b80d980604
Tristan Macell <tristanmacelli@gmail.com>
2020-05-11 21:24:19 -0700

    [ADD] Descriptions to variables

--- modules/build_server/variables.tf
+++ modules/build_server/variables.tf
@@ -1,6 +1,10 @@
 variable "jenkins" {
   description = "Private key for ec-2 running jenkins"
+  type        = string
 }
 
-variable "process_messages_bucket_arn" {}
+variable "process_messages_bucket_arn" {
+  description = "ARN of the S3 bucket used to deploy code to"
+  type        = string
+}
 
