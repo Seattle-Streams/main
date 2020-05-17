39a5986eca04b5e901e7427ef74bdc3cdbdeeea2
Tristan Macell <tristanmacelli@gmail.com>
2020-05-17 13:46:48 -0700

    [ADD] Content-Type to endpoint response

--- modules/endpoint_response/main.tf
+++ modules/endpoint_response/main.tf
@@ -1,13 +1,18 @@
 resource "aws_api_gateway_method_response" "response" {
-  rest_api_id = "${var.api_id}"
-  resource_id = "${var.resource_id}"
-  http_method = "${var.http_method}"
-  status_code = "${var.status_code}"
+  rest_api_id         = "${var.api_id}"
+  resource_id         = "${var.resource_id}"
+  http_method         = "${var.http_method}"
+  status_code         = "${var.status_code}"
+  response_parameters = { "method.response.header.Content-Type" = "text/richtext" }
 }
 resource "aws_api_gateway_integration_response" "integration_response" {
-  rest_api_id      = "${var.api_id}"
-  resource_id      = "${var.resource_id}"
-  http_method      = "${var.http_method}"
-  status_code      = "${var.status_code}"
-  content_handling = "${var.content_handling}"
+  rest_api_id         = "${var.api_id}"
+  resource_id         = "${var.resource_id}"
+  http_method         = "${var.http_method}"
+  status_code         = "${var.status_code}"
+  content_handling    = "${var.content_handling}"
+  response_parameters = { "method.response.header.Content-Type" = "text/richtext" }
+  response_templates = {
+    "text/richtext" = "Message Received"
+  }
 }
