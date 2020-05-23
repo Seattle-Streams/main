6291442cb3dc46941fc30e400217585ac8907f09
Tristan Macell <tristanmacelli@gmail.com>
2020-05-22 23:51:36 -0700

    [CHANGE] point twilio_lambda invoke url to prod alias

--- modules/twilio_integration/output.tf
+++ modules/twilio_integration/output.tf
@@ -1,5 +1,5 @@
 output "lambda_invoke_arn" {
-  value = "${aws_lambda_function.twilio_lambda.invoke_arn}"
+  value = "${aws_lambda_function.twilio_lambda.production_alias.arn}"
 }
 
 output "lambda_function_name" {
